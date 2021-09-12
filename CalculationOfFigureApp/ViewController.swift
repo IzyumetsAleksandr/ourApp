b1023d22c02cec31824735c82e44a5175b6188cd
Izyumets Aleksandr <izyumets.a@yandex.ru>
2021-09-12 21:56:03 +0300

    Initial Commit

---
+++ CalculationOfFigureApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculationOfFigureApp
+//
+//  Created by Izyumets Aleksandr on 12.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
